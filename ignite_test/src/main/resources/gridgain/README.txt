5940e0251bb254e7e961af8ba7f797b021dbb56d
Luodian <1121058986@qq.com>
2018-01-02 15:26:10 +0800

    added ignite

---
+++ ignite_test/src/main/resources/gridgain/README.txt
@@ -0,0 +1,26 @@
+Apache Ignite In-Memory Data Fabric
+===================================
+
+Ignite In-Memory Data Fabric is designed to deliver high performance for the widest
+array of in-memory computing use cases.
+
+The main feature set of Ignite In-Memory Data Fabric includes:
+* Advanced Clustering
+* Compute Grid
+* Data Grid
+* SQL Database
+* Service Grid
+* IGFS - Ignite File System
+* Distributed Data Structures
+* Distributed Messaging
+* Distributed Events
+* Streaming & CEP
+
+For information on how to get started with Apache Ignite please visit:
+
+    http://apacheignite.readme.io/docs/getting-started
+
+
+You can find Apache Ignite documentation here:
+
+    http://apacheignite.readme.io/docs
---
+++ ignite_test/target/classes/gridgain/bin/include/parseargs.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+#
+# Licensed to the Apache Software Foundation (ASF) under one or more
+# contributor license agreements.  See the NOTICE file distributed with
+# this work for additional information regarding copyright ownership.
+# The ASF licenses this file to You under the Apache License, Version 2.0
+# (the "License"); you may not use this file except in compliance with
+# the License.  You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+#
+# Parses command line parameters into Ignite variables that are common for the launcher scripts:
+# CONFIG
+# INTERACTIVE
+# QUIET
+# JVM_XOPTS
+# NOJMX
+#
+# Script setups reasonable defaults (see below) for omitted arguments.
+#
+# Scripts accepts following incoming variables:
+# DEFAULT_CONFIG
+#
+# Can be used like:
+#       . "${IGNITE_HOME}"/bin/include/parseargs.sh
+# in other scripts to parse common command lines parameters.
+#
+
+CONFIG=${DEFAULT_CONFIG}
+INTERACTIVE="0"
+NOJMX="0"
+QUIET="-DIGNITE_QUIET=true"
+JVM_XOPTS=""
+
+while [ $# -gt 0 ]
+do
+    case "$1" in
+        -i) INTERACTIVE="1";;
+        -nojmx) NOJMX="1";;
+        -v) QUIET="-DIGNITE_QUIET=false";;
+        -J*) JVM_XOPTS="$JVM_XOPTS ${1:2}";;
+        *) CONFIG="$1";;
+    esac
+    shift
+done
